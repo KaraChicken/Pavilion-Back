16e7a4958342be048551213068e5d266e75785cd
KaraChicken <ed29470381@gmail.com>
2024-02-01 16:30:16 +0800

    登入、登出、購物車增改刪查

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,6 @@
-import porducts from '../models/products.js'
-import { StatusCodes } from 'http-status-codes'
-import calidator from 'calidator'
 import products from '../models/products.js'
+import { StatusCodes } from 'http-status-codes'
+import validator from 'validator'
 
 export const create = async (req, res) => {
   try {
@@ -33,7 +32,7 @@ export const getAll = async (req, res) => {
   try {
     const sorBy = req.query.sortBy || 'createdAt'
     const sorOrder = parseInt(req.query.sortOrder) || -1
-    const itemsPerPage =parseInt(req.query.itemsPerPage) || 20
+    const itemsPerPage = parseInt(req.query.itemsPerPage) || 20
     const page = parseInt(req.query.page) || 1
     const regex = new RegExp(req.query.search || '', 'i')
 
@@ -65,6 +64,7 @@ export const getAll = async (req, res) => {
       }
     })
   } catch (error) {
+    console.log(error)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: '未知錯誤'
@@ -121,19 +121,69 @@ export const getId = async (req, res) => {
   try {
     if (!validator.isMongoId(req.params.id)) throw new Error('ID')
 
+    const result = await products.findById(req.parms.id)
+
+    if (!result) throw new Error('NOT FOUND')
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result
+    })
+  } catch (error) {
+    if (error.name === 'CastError' || error.message === 'ID') {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: 'ID 格式錯誤'
+      })
+    } else if (error.message === 'NOT FOUND') {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: '查無商品'
+      })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '未知錯誤'
+      })
+    }
+  }
+}
+
+export const edit = async (req, res) => {
+  try {
+    if (!validator.isMongoId(req.params.id)) throw new Error('ID')
+
     req.body.image = req.file?.path
-    await products.findByIdAndUpdate(req.params.id, req.body, { runValidators: true }).orFail(new Error('NOT FOUND'))
+    await products.findByIdAndUpdate(req.paramsid, req.body, { runValidators: true }).orFail(new Error('NOT FOUND'))
 
     res.status(StatusCodes.OK).json({
       success: true,
       message: ''
     })
   } catch (error) {
-    if(error.name === 'CastError' || error.message === 'ID') {
-      res.status(StatusCodes.OK).json({
+    if (error.name === 'CastError' || error.message === 'ID') {
+      res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
         message: 'ID 格式錯誤'
       })
-    } else if (error.message === 'NO')
+    } else if (error.message === 'NOT FOUND') {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: '查無商品'
+      })
+    } else if (error.message === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message
+      })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '未知錯誤'
+      })
+    }
   }
 }
